0a1f67f62b7932177cb82f428ba804600b6916d2
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-07 23:36:22 +0000

    Added vpc.tf

---
+++ Project_1/terraform/vpc.tf
@@ -0,0 +1,25 @@
+# VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = [var.az1, var.az2, var.az3]
+  private_subnets = [var.private_sub1, var.private_sub2, var.private_sub3]
+  public_subnets  = [var.public_sub1, var.public_sub2, var.public_sub3]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "Dev"
+  }
+
+  vpc_tags = {
+    Name = var.vpc_name
+  }
+}
