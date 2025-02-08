27b1093a0f7b936ba33ff401bce4c5df2a026a93
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-08 00:14:26 +0000

    Added rds.tf

---
+++ Project_1/terraform/rds.tf
@@ -0,0 +1,29 @@
+# Deploy RDS for MySQL
+resource "aws_db_instance" "mysql" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7.44"
+  instance_class         = "db.t3.micro"
+  name                   = var.dbname
+  username               = var.dbuser
+  password               = var.dbpass
+  parameter_group_name   = "default.mysql5.6"
+  multi_az               = "false"
+  publicly_accessible    = "false"
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "mysql-subnet-group"
+  subnet_ids = [
+    module.vpc.private_subnets[0], 
+    module.vpc.private_subnets[1], 
+    module.vpc.private_subnets[2]
+    ]
+  tags = {
+    Name = "Subnet group for RDS"
+  }
+}
