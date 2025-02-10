be731f01628599260c617004e93ecb822e1de107
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-10 20:12:01 +0000

    Update S3 backend key for Terraform state management

--- Project_1/terraform/tstate_s3.tf
+++ Project_1/terraform/tstate_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "aws-projects-tf-state"
-    key            = "terraform/terraform.tfstate"
+    key            = "terraform"
     region         = "us-east-1"
     dynamodb_table = "aws-projects-tfstate-lock"
   }
