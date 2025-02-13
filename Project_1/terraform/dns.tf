113f15029c1e5bb4c23af0244b1edfb6b9572415
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-13 20:46:12 +0000

    Add DNS record for MySQL instance and update documentation in dns.tf

--- Project_1/terraform/dns.tf
+++ Project_1/terraform/dns.tf
@@ -1,4 +1,6 @@
+# Purpose: This file is used to create the DNS records for the internal resources.
 
+# The DNS records are created for the following resources:
 resource "aws_route53_zone" "internal" {
   name = "saturn.local"
   vpc {
@@ -35,4 +37,14 @@ resource "aws_route53_record" "memcached" {
   records = [aws_elasticache_cluster.memcached.cache_nodes.0.address]
 
   depends_on = [aws_elasticache_cluster.memcached]
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = aws_route53_zone.internal.zone_id
+  name    = "mysql.saturn.local"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_db_instance.mysql.address]
+
+  depends_on = [aws_db_instance.mysql]
 }
