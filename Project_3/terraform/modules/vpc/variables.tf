e876ff7571da0542bb9a1af9f5ce265a10727873
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-19 23:06:13 +0000

    Add VPC module with subnets, route tables, and NAT gateway configuration

---
+++ Project_3/terraform/modules/vpc/variables.tf
@@ -0,0 +1,24 @@
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
