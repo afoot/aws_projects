7f29f5041b00fba32a42dedbfdda38ed0624474e
Intourist <19631339+afoot@users.noreply.github.com>
2025-02-27 22:37:47 +0000

    Add Terraform configuration for VPC, EC2 instance, and security group

---
+++ Project_5_Provisioning EC2s on VPC/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "A list of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "A list of private subnets for the client app"
+  value       = module.vpc.private_subnets
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "A list of private subnets CIDRs"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "A list of public subnets CIDRs"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
+output "public_ec2_ips" {
+  description = "A list of public IPs for the EC2 instances"
+  value       = aws_instance.web.*.public_ip
+  }
