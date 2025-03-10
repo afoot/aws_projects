1d53b77f5a2c1d4c8d2b1c79287c10b088456fc9
Intourist <19631339+afoot@users.noreply.github.com>
2025-03-10 22:38:26 +0000

    Add initial Flask application structure with database setup, templates, and styling

---
+++ Project_9/outputs.tf
@@ -0,0 +1,21 @@
+# VPC
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  description = "A list of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  description = "A list of private subnets for the client app"
+  value       = module.vpc.private_subnets
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "A list of private subnets CIDRs"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
---
+++ Project_9/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ecr_repo_name" {
+  description = "The ECR repository URL"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "The ECR repository name"
+  type        = string
+}
